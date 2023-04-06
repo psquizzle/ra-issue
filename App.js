8b6372de2d47a45f9853382a222b4ca6de5335d7
psquizzle <53052232+psquizzle@users.noreply.github.com>
2023-04-06 13:29:34 +0200

    first

--- App.js
+++ App.js
@@ -1,11 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button } from 'react-native';
+import Animated, { Easing, Extrapolate, Extrapolation, interpolate, interpolateNode, runOnJS, useAnimatedGestureHandler, useAnimatedProps, useAnimatedScrollHandler, useAnimatedStyle, useDerivedValue, useSharedValue, withDecay, withSpring, withTiming } from "react-native-reanimated"
+import { Audio } from "expo-av";
+import React from 'react';
 export default function App() {
+  const [isRecording, setIsRecording] = React.useState(false);
+  const [recording, setRecording] = React.useState();
+  const meteringSharedValue = useSharedValue(-160);
+  const widthCircle = useSharedValue(1);
+  const onRecordingStatusUpdate = (recordStatus) => {
+    if (recordStatus.metering) {
+      //console.log(recordStatus.metering)
+    meteringSharedValue.value = recordStatus.metering;
+    }
+  };
+
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const interpolatedValue = interpolate(
+      meteringSharedValue.value,
+      [-160, -60, 0],
+      [0, 30, 120],
+      { extrapolateRight: Extrapolation.CLAMP }
+    );
+    widthCircle.value = withSpring(interpolatedValue)
+    return {
+      width: widthCircle.value,
+      height: widthCircle.value,
+      borderRadius: widthCircle.value / 2,
+    };
+  },[meteringSharedValue]);
+
+  async function stopRecording() {
+
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    setIsRecording(false);
+
+    await recording.stopAndUnloadAsync();
+  }
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+        
+      );
+      await recording.setProgressUpdateInterval(20);
+      await recording.setOnRecordingStatusUpdate(onRecordingStatusUpdate);
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={{height:250}}>
+          <Animated.View style={[{backgroundColor:'red'},animatedStyle]}></Animated.View>
+        </View>
+      <Button onPress={startRecording} title='Start Recoding'></Button>
+      <Button onPress={stopRecording} title='Stop Recoding'></Button>
     </View>
   );
 }
